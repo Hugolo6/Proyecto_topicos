d133f531d31e4365f01d1624b76e3d67855839ab
Hugolo6 <pacom6985@gmail.com>
2026-04-10 22:21:53 -0600

    first commit

---
+++ app/src/main/java/com/example/proyecto/InicioSesionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.proyecto
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class InicioSesionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio_sesion)
+    }
+}
